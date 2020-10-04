b0c474fdb3151af0b090234819e12ea0b2f1cbae
alageev <alageev01@gmail.com>
2020-10-05 01:00:29 +0300

    Initial Commit

---
+++ Jungle/JungleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JungleApp.swift
+//  Jungle
+//
+//  Created by Алексей Агеев on 05.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct JungleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
